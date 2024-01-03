53c412cd6998bdfb6f9921e7e1b8d434edea5873
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-01-03 20:53:54 +0530

    filter(find) and if todo exists,  update/delete the filtered Todo, else raise exception

--- api/routes/todo.py
+++ api/routes/todo.py
@@ -10,7 +10,28 @@ async def all_todo():
 
 @todo_router.post("/")
 async def post_todo(body: PostTodo):
+# PostTodo validates input
     data = body.model_dump(exclude_unset=True)
+    # and exlude fields that are optional and not set,
     row = await Todo.create(data)
+    # using tortoise-orm to create the todo
     return await GetTodo.from_tortoise_orm(row)
+    # and GetTodo to return the todo created
+
+@todo_router.put("/{key}")
+async def update_todo(key: int,body:PutTodo):
+    data = body.model_dump(exclude_unset=True)
+    exists = await Todo.filter(id=key).exists()
+    if not exists:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+    await Todo.filter(id=key).update(**data)
+    return await GetTodo.from_queryset_single(Todo.get(id=key))
+    
+@todo_router.delete("/{key}")
+async def delete_todo(key: int):
+    exists = await Todo.filter(id=key).exists()
+    if not exists:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+    await Todo.filter(id=key).delete()
+    return "Todo deleted successfully."
     
